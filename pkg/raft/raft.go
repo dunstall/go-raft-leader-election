b9acb457ec8dab3483e12f212113a715ef2aeafc
Andy Dunstall <andydunstall@hotmail.com>
2020-07-19 03:53:00 -0400

    Define Raft RPC's

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -1,4 +1,18 @@
 package raft
 
+import (
+	"log"
+
+	"github.com/dunstall/goraft/pkg/pb"
+)
+
 type Raft struct {
+}
+
+func (r *Raft) Election() {
+	req := pb.RequestVoteRequest{
+		Term:        0,
+		CandidateId: 0,
+	}
+	log.Println(req.Term)
 }
