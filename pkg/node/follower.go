22b90143ef3b599cd316fd78ff1e1e9853aaf1e0
Andy Dunstall <andydunstall@hotmail.com>
2020-07-21 12:48:24 -0400

    Dont export follower

--- pkg/node/follower.go
+++ pkg/node/follower.go
@@ -5,28 +5,28 @@ import (
   "github.com/golang/glog"
 )
 
-type Follower struct {
+type follower struct {
 }
 
 func NewFollower() nodeState {
-	return &Follower{}
+	return &follower{}
 }
 
-func (f *Follower) Expire(node *Node) {
+func (f *follower) Expire(node *Node) {
 	glog.Info("follower: node timed out")
 	node.setState(node.candidateState())
 }
 
-func (f *Follower) Elect(node *Node) {
+func (f *follower) Elect(node *Node) {
 	glog.Warning("follower: cannot elect a follower")
 }
 
-func (f *Follower) VoteRequest(node *Node, cb server.Callback) {
+func (f *follower) VoteRequest(node *Node, cb server.Callback) {
 	// TODO(AD)
 	glog.Info("follower: received vote request")
 }
 
-func (f *Follower) AppendEntriesRequest(node *Node) {
+func (f *follower) AppendEntriesRequest(node *Node) {
 	// TODO(AD)
 	glog.Info("follower: received append entries request")
 }
