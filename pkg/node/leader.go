171d05e013a0310f9b96ac37d312a4dadaede4ba
Andy Dunstall <andydunstall@hotmail.com>
2020-07-22 11:53:11 -0400

    Add leader node state

--- pkg/node/leader.go
+++ pkg/node/leader.go
@@ -1,9 +1,8 @@
 package node
 
 import (
-	"log"
-
 	"github.com/dunstall/goraft/pkg/server"
+	"github.com/golang/glog"
 )
 
 type leader struct {
@@ -14,21 +13,31 @@ func NewLeader() nodeState {
 }
 
 func (l *leader) Expire(node *Node) {
-	// TODO(AD)
-	log.Println("leader: node timed out")
+	glog.Infof("leader: node timed out in term %d", node.Term())
+	node.SetTerm(node.Term() + 1)
+	node.setState(node.followerState())
 }
 
 func (l *leader) Elect(node *Node) {
-	// TODO(AD)
-	log.Println("leader: leader already elected")
+	glog.Warning("leader: leader already elected in term %d", node.Term())
 }
 
-func (l *leader) VoteRequest(node *Node, cb server.VoteRequest) {
-	// TODO(AD)
-	log.Println("leader: leader received vote request")
+func (l *leader) VoteRequest(node *Node, req server.VoteRequest) {
+	glog.Infof("leader: received vote request from candidate %d", req.CandidateID())
+
+	if req.Term() > node.Term() {
+		glog.Infof("leader: vote request has greater term %d - reverting to follower", req.Term())
+
+		node.setState(node.followerState())
+		node.VoteRequest(req)
+	} else {
+		req.Deny()
+
+		glog.Infof("leader: denied vote request with term %d", req.Term())
+	}
 }
 
 func (l *leader) AppendEntriesRequest(node *Node) {
 	// TODO(AD)
-	log.Println("leader: leader received append entries request")
+	glog.Infof("leader: leader received append entries request")
 }
---
+++ pkg/node/leader_test.go
@@ -0,0 +1,98 @@
+package node
+
+import (
+	"testing"
+
+	"github.com/dunstall/goraft/pkg/server/mock_server"
+	"github.com/golang/mock/gomock"
+)
+
+func TestLeaderExpire(t *testing.T) {
+	node := NewNode()
+	node.setState(node.leaderState())
+
+	node.Expire()
+	if node.state != node.followerState() {
+		t.Error("expected node to be in follower state")
+	}
+
+	// The node should have entered a new term.
+	var expected uint32 = 2
+	actual := node.Term()
+	if actual != expected {
+		t.Errorf("node.Term() != %d, actual %d", expected, actual)
+	}
+}
+
+func TestLeaderElect(t *testing.T) {
+	node := NewNode()
+	node.setState(node.leaderState())
+
+	node.Elect()
+	if node.state != node.leaderState() {
+		t.Error("expected node to be in leader state")
+	}
+
+	// The term should not have changed.
+	var expected uint32 = 1
+	actual := node.Term()
+	if actual != expected {
+		t.Errorf("node.Term() != %d, actual %d", expected, actual)
+	}
+}
+
+func TestLeaderVoteRequestTermGreater(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	node := NewNode()
+	node.setState(node.leaderState())
+
+	var newTerm uint32 = node.Term() + 1
+	var candidateID uint32 = 0xff
+
+	mockreq := mock_server.NewMockVoteRequest(ctrl)
+	mockreq.EXPECT().CandidateID().AnyTimes().Return(candidateID)
+	mockreq.EXPECT().Term().AnyTimes().Return(newTerm)
+
+	// As the term is greater than the node should grant and call back to
+	// follower in new term.
+	mockreq.EXPECT().Grant()
+	node.VoteRequest(mockreq)
+
+	if node.state != node.followerState() {
+		t.Error("expected node to be in follower state")
+	}
+
+	term := node.Term()
+	if term != newTerm {
+		t.Errorf("node.Term() != %d, actual %d", newTerm, term)
+	}
+}
+
+func TestLeaderVoteRequestTermEqual(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	node := NewNode()
+	node.setState(node.leaderState())
+
+	var newTerm uint32 = node.Term()
+	var candidateID uint32 = 0xff
+
+	mockreq := mock_server.NewMockVoteRequest(ctrl)
+	mockreq.EXPECT().CandidateID().AnyTimes().Return(candidateID)
+	mockreq.EXPECT().Term().AnyTimes().Return(newTerm)
+
+	// As the term is equal deny the request and dont change the current state.
+	mockreq.EXPECT().Deny()
+	node.VoteRequest(mockreq)
+
+	if node.state != node.leaderState() {
+		t.Error("expected node to be in leader state")
+	}
+	term := node.Term()
+	if term != newTerm {
+		t.Errorf("node.Term() != %d, actual %d", newTerm, term)
+	}
+}
