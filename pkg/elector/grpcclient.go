1b6d4f6eaae672262999675c66f1342407b528ae
Andy Dunstall <andydunstall@hotmail.com>
2020-07-24 12:35:29 -0400

    Add grpc client

---
+++ pkg/elector/grpcclient.go
@@ -0,0 +1,17 @@
+package elector
+
+import (
+	"github.com/golang/glog"
+	"google.golang.org/grpc"
+)
+
+type GRPCClient struct{}
+
+func (c *GRPCClient) Dial(addr string) Connection {
+	// TODO(AD) Ensure this is all background so should never return an error.
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		glog.Fatalf("")
+	}
+	return NewGRPCConnection(conn)
+}
