643a62006f93df97e1a5ea669178b3d9baf0998b
Andy Dunstall <andydunstall@hotmail.com>
2020-07-25 03:21:03 -0400

    Fix log format

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ func parseNodeID() uint32 {
 	id := flag.Uint("id", 0, "node ID")
 	flag.Parse()
 	if *id == 0 || *id > 0xFFFFFFFF {
-		log.Fatal("bad node ID: %d", *id)
+		log.Fatalf("bad node ID: %d", *id)
 	}
 	return uint32(*id)
 }
