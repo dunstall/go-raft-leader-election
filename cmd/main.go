173a76cbafd884f9a172d77be9bdc7e2301a15d7
Andy Dunstall <andydunstall@hotmail.com>
2020-07-25 03:20:41 -0400

    Cleanup cmd

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,15 @@ import (
 	"github.com/dunstall/goraft/pkg/raft"
 )
 
-func main() {
+func parseNodeID() uint32 {
 	id := flag.Uint("id", 0, "node ID")
 	flag.Parse()
-
-	if *id == 0 {
-		log.Println("bad id")
+	if *id == 0 || *id > 0xFFFFFFFF {
+		log.Fatal("bad node ID: %d", *id)
 	}
+	return uint32(*id)
+}
 
-	raft.Run(uint32(*id))
+func main() {
+	raft.Run(parseNodeID())
 }
